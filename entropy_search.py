698a216e41cf17d892c9082085af9a41ebf7764c
Riccardo Malavolti <riccardo.malavolti@stud.unifi.it>
2022-08-23 15:45:33 +0200

    adds a main test method

--- entropy_search.py
+++ entropy_search.py
@@ -1,26 +1,35 @@
-from random import random
+from distutils.command.config import config
+from random import random, sample
 from statistics import correlation
 from emcee import EnsembleSampler
 import epmgp
 import numpy as np
 import scipy
+import time
 import scipy.stats as sts
 from scipy.optimize import minimize
 from tensorflow_probability import distributions as tfp
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import Matern, WhiteKernel
-
+from sklearn.svm import SVC
+from sklearn.metrics import zero_one_loss
+from tensorflow.keras.datasets import mnist
 import math
 
 
 covariance_prior_mean, covariance_prior_sigma = 1, 0
 
 def sample_hypers(X, y):
-    # Priors:
-    # lambda = uniform [-10, 2]
-    # covariance amplitude = lognormal(0, 1)
-    # noise variance = horseshoe with length 0.1
-    
+    """
+        Samples from the hyperparameter space using MCMC
+
+        #### Priors:
+        - lambda = uniform [-10, 2]
+        - covariance amplitude = lognormal(0, 1)
+        - noise variance = horseshoe with length 0.1
+
+    """
+
     cov = 1
     kernel = cov * Matern() + WhiteKernel()
     hyper_distribution = GaussianProcessRegressor(kernel=kernel)
@@ -101,17 +110,20 @@ def compute_innovations(x, model, representer_points, variance, noise):
     """
     var_noise = 0.5*10**(-3) # the noise is estimated as very little in comparison to the GP variance
     # Compute the correlation matrix and get the element corresponding to x
-    _, correlation_x_r = model.predict(np.concatenate(x, representer_points), return_cov=True)[-1, :-1]
+    _, correlation_x_r = model.predict(np.concatenate((x, representer_points)), return_cov=True)
+    correlation_x_r = correlation_x_r[-1, :-1]
     d_mu = np.convolve(np.convolve(correlation_x_r, variance), np.dot(np.linalg.cholesky(variance + var_noise), noise))
-    
+    # FIXME Cholesky decomposition must be computed on a matrix NxN, but `variance` is a vector
     d_sigma = np.convolve(np.convolve(correlation_x_r, variance), np.transpose(correlation_x_r))
 
     return d_mu, d_sigma
 
 def entropy_search(dataset):
+    
+    print("Sampling hypeparameters..")
     # K samples with mcmc over GP hyperparameters: lambda, covariance amplitude for Matérn kernel + noise variance
-    hyperparameters = sample_hypers()
-    n_gen_samples = 20
+    hyperparameters = sample_hypers(dataset["X"], dataset["y"])
+    n_gen_samples = 50
     P = 20
 
     Omega = []
@@ -121,27 +133,30 @@ def entropy_search(dataset):
     representers = []
 
     for hyper in hyperparameters:
-        cov, lamb, noise = hyper
+        cov, lamb, noise = np.e**(hyper) # Convert from log scale
         kernel = cov * Matern(length_scale=lamb) + WhiteKernel(noise_level=noise)
-        regressor = GaussianProcessRegressor(kernel=kernel).fit(dataset["X"], dataset["y"])
+        regressor = GaussianProcessRegressor(kernel=kernel, optimizer=None).fit(dataset["X"], dataset["y"])
         models.append(regressor)
 
         # sample Z point from M and get predictive mean + covariance
-        X_samples = np.random.rand((n_gen_samples, dataset["X"].shape[0]))
+        X_samples = np.random.rand(n_gen_samples, dataset["X"].shape[1])
         representers.append(X_samples)
         mean, cov = regressor.predict(X_samples, return_cov=True)
 
         # compute their Expected Improvement
+        print("Computing EI...")
         exp_improvement = expected_improvement(mean, cov, regressor.sample_y(X_samples))
-        U.append[exp_improvement]
+        U.append(exp_improvement)
 
         # Compute p_min using EPMGP
+        print("Computing pMin")
         p_min.append(compute_pmin(mean, cov))
         
         # generate P noise vectors from a Gaussian(0, I_Z)
         # FIXME: noise vectors should not be deterministic
         # also why save'em in memory when they can be generated on the fly?
         innovations = []
+        print("Generating innovations..")
         for _ in range(P):
             # Generate a gaussian noise vector
             innovations.append(sts.norm.ppf(np.linspace(0, 1, P + 2)[1:-1]))
@@ -151,14 +166,14 @@ def entropy_search(dataset):
     def information_gain(test_point):
         """
         Returns the information gain value for `test_point`
+
         """
         a = 0
         for i, model in enumerate(models):
-            _, testpoint_var = model.predict(test_point, return_std=True)
-            testpoint_var = testpoint_var**2
+            _, testpoint_var = model.predict(test_point.reshape(1, -1), return_cov=True)
 
             for _ in range(P):
-                d_mu, d_sigma = compute_innovations(test_point, model, representers[i], testpoint_var, Omega[i])
+                d_mu, d_sigma = compute_innovations(test_point.reshape(1, -1), model, representers[i], testpoint_var, Omega[i])
 
                 # Compute pmin from the updated posterior
                 q_min = compute_pmin(mean + d_mu, cov + d_sigma)
@@ -167,10 +182,104 @@ def entropy_search(dataset):
                     np.sum(p_min[i] * (np.log(p_min[i] + U[i])))
 
                 a += 1/P * d_entropy
+
+        print(f"IG: {1/len(models) * a}")  
         return 1/len(models) * a
 
     # maximize information gain => minimize -information_gain()
     # FIXME: find a better strategy for the initial guess/guesses. 
     # Maybe random + last good configuration?
-    return minimize(fun=lambda x: -information_gain(x), x0=[0., 0., 0.])
+    print("Ready to optimize Information Gain")
+    return minimize(fun=lambda x: -information_gain(x), x0=dataset["X"][np.argmax(dataset["y"])])
 
+def obj_function(configuration, dataset):
+    c, gamma = configuration
+    classifier = SVC(C=c, gamma=gamma).fit(dataset["X"], dataset["y"])
+    predictions = classifier.predict(dataset["X_test"])
+    return zero_one_loss(dataset["y_test"], predictions)
+
+
+def generate_prior(obj_function, dataset):
+
+    C = np.array([1, 10, 100])
+    gamma = np.array([10**(-3), 10**(-5), 10**(-7)])
+
+    x_values, y_values = [], []
+
+    for c in C:
+        for g in gamma:
+            y = obj_function((c,g), dataset)
+            x_values.append((c,g))
+            y_values.append(y)
+
+    return np.array(x_values), np.array(y_values)
+
+def load_mnist(training_size):
+    dataset = {}
+
+    (train_x, train_y), (test_x, test_y) = mnist.load_data()
+    train_x = train_x.reshape(train_x.shape[0], train_x.shape[1] * train_x.shape[2])
+    test_x = test_x.reshape(test_x.shape[0], test_x.shape[1] * test_x.shape[2])
+
+    indices = sample(range(1, train_x.shape[0]), math.floor(train_x.shape[0]/training_size))
+
+    dataset["X"] = train_x[indices]
+    dataset["y"] = train_y[indices]
+
+    dataset["X_test"] = test_x
+    dataset["y_test"] = test_y
+
+    return dataset
+
+def main():
+    """
+        Test Entropy Search with a simple "complex" function.
+    """
+    iterations = 30
+
+    data = load_mnist(128)
+
+    best_x = None
+    best_y = None
+
+    # Bayesian optimization needs a prior: this can be
+    # derived by some knowledge about the function
+    # or generated/sampled by some fancy strategy
+    dataset = {}
+    dataset["X"], dataset["y"] = generate_prior(obj_function, data)
+    print("Prior generated")
+
+    # Optimization loop can finally start. The stopping criteria is
+    # based on a fixed number of iterations but could take in account
+    # a "min improvement" policy 
+
+    for _ in range(iterations):
+
+        # Find the next candidate
+        candidate = entropy_search(dataset)
+
+        print("Evaluating function..")
+        function_time = time.time()
+        # Evaluate the function
+        y = obj_function(candidate, data)
+        function_time = time.time() - function_time
+
+        performance = "-" if best_y is not None else str((y / best_y - 1)*100 )
+        print(f"Function value: {y} ({performance}%), {function_time}s")
+
+        # Save the results
+        dataset["X"].append(candidate)
+        dataset["y"].append(y)
+
+        # Save the best candidate so far
+        best_index = np.argmin(dataset["y"])
+        best_x = dataset["X"][best_index]
+        best_y = dataset["y"][best_index]
+    
+
+    # Optimization loop has ended, print the results
+    print(f"Best score {best_y}")
+    print(f"with configuration: {str(best_x)}")
+
+if __name__ == "__main__":
+    main()
