ed2e7559bd5e3d0d24143e612f8ed704061c6bf4
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2022-09-16 20:19:20 +0200

    implements implements latest changes

--- entropy_search.py
+++ entropy_search.py
@@ -8,9 +8,11 @@ import scipy
 import time
 import scipy.stats as sts
 from scipy.optimize import minimize
-from tensorflow_probability import distributions as tfp
+#from tensorflow_probability import distributions as tfp
+from horseshoe import Horseshoe
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import Matern, WhiteKernel
+from sklearn.gaussian_process.kernels import  WhiteKernel
+from ard import AutomaticRelevanceDetermination
 from sklearn.svm import SVC
 from sklearn.metrics import zero_one_loss
 from tensorflow.keras.datasets import mnist
@@ -19,7 +21,7 @@ import logging
 
 covariance_prior_mean, covariance_prior_sigma = 1, 0
 
-def sample_hypers(X, y):
+def sample_hypers(X, y, K=20):
     """
         Samples from the hyperparameter space using MCMC
 
@@ -31,7 +33,7 @@ def sample_hypers(X, y):
     """
 
     cov = 1
-    kernel = cov * Matern() + WhiteKernel()
+    kernel = cov * AutomaticRelevanceDetermination(nu=5/2) + WhiteKernel()
     hyper_distribution = GaussianProcessRegressor(kernel=kernel)
     hyper_distribution.fit(X, y)
 
@@ -43,15 +45,26 @@ def sample_hypers(X, y):
 
         prior = 0
 
+        # Check first if all parameters are inside some reasonable bounds.
+        # This is mostly due to avoid silly choices that could be ammissible
+        # but numerically useless.
+
+        if not 0 < cov < 100:
+            return -np.inf
+
         # Log likelihood for lambda is 0 when lambda is inside its bounds
         if not -9 < lamb < 2:
+            return -np.inf
+
+        if not -20 < noise < 20:
             return -np.inf
 
         # Log probability for lognormal distribution
         prior += sts.lognorm.logpdf(cov, covariance_prior_mean, covariance_prior_sigma)
 
         # Horseshoe 
-        prior += tfp.Horseshoe(scale=0.1).log_prob(noise).numpy()
+        #prior += tfp.Horseshoe(scale=0.1).log_prob(noise).numpy()
+        prior += Horseshoe(scale=0.1).logpdf(noise)
 
         # Compute log likelihood of the current distribution with proposed values
 
@@ -59,21 +72,23 @@ def sample_hypers(X, y):
 
         return prior + prob
 
-    nwalkers, ndim, iterations = 20, kernel.n_dims, 50
+    nwalkers, ndim, iterations = K, kernel.n_dims, 1000
     sampler = EnsembleSampler(nwalkers, ndim, log_prob)
-    sampler.run_mcmc(np.random.rand(nwalkers, ndim), iterations, rstate0=np.random.get_state())
+    state = sampler.run_mcmc(np.random.rand(nwalkers, ndim), 100, rstate0=np.random.get_state())
+    sampler.reset()
+    sampler.run_mcmc(state, iterations)
     return sampler.chain[:, -1]
 
-def expected_improvement(mean, covariance, y_values):
+def expected_improvement(mean, covariance, y_values, exploration=0, ):
     y_max = y_values.max()
 
-    if covariance.all() == 0:
-        logging.warning("Covariance is zero.")
+    # Since we need sigma(x) we just use the diagonal
+    variance = np.sqrt(np.diag(covariance))
 
-    u = (mean - y_max) / covariance
-    ei = covariance * (u * sts.norm.cdf(u) + sts.norm.pdf(u))
+    u = (mean - y_max - exploration) / variance
+    ei = variance * (u * sts.norm.cdf(u) + sts.norm.pdf(u))
 
-    ei[covariance <= 0.] = 0.
+    ei[variance <= 0.] = 0.
 
     return ei
 
@@ -118,7 +133,7 @@ def compute_innovations(x, model, representer_points, variance):
         - `d_mu`
         - `d_sigma`
     """
-    var_noise = np.array([[0.5*10**(-4)]]) # the noise is estimated as very little in comparison to the GP variance
+    var_noise = np.array([[10**(-4)]]) # the noise is estimated as very little in comparison to the GP variance
     # Compute the correlation matrix and get the element corresponding to x
     _, correlation_x_r = model.predict(np.concatenate((x, representer_points)), return_cov=True)
     correlation_x_r = (correlation_x_r[-1, :-1]).reshape(-1, 1) # vector (n_rep , 1)
@@ -129,12 +144,14 @@ def compute_innovations(x, model, representer_points, variance):
     return d_mu, d_sigma
 
 def entropy_search(dataset):
+    n_hyper_samples = 20    # K parameter
+    n_gen_samples = 50      # Z parameter
+    n_innovations = 20      # P parameter
     
     logging.info("Sampling hypeparameters..")
     # K samples with mcmc over GP hyperparameters: lambda, covariance amplitude for Matérn kernel + noise variance
-    hyperparameters = sample_hypers(dataset["X"], dataset["y"])
-    n_gen_samples = 30
-    P = 20
+    hyperparameters = sample_hypers(dataset["X"], dataset["y"], K=n_hyper_samples)
+    
 
     Omega = []
     p_min = []
@@ -146,12 +163,12 @@ def entropy_search(dataset):
 
     for hyper in hyperparameters:
         kernel_cov, lamb, noise = hyper # Convert from log scale
-        kernel = kernel_cov * Matern(length_scale=math.e**lamb) + WhiteKernel(noise_level=noise)
+        kernel = kernel_cov * AutomaticRelevanceDetermination(length_scale=math.e**lamb, nu=5/2) + WhiteKernel(noise_level=abs(noise))
         regressor = GaussianProcessRegressor(kernel=kernel, optimizer=None).fit(dataset["X"], dataset["y"])
         models.append(regressor)
 
         # sample Z point from M and get predictive mean + covariance
-        X_samples = np.random.normal(size=(n_gen_samples, dataset["X"].shape[1])) # Should I sample from all the dataset space?
+        X_samples = np.random.uniform(low=-10, high=10, size=(n_gen_samples, dataset["X"].shape[1])) # FIXME: sample from the parameter space, using custom function
         representers.append(X_samples)
         mean, cov = regressor.predict(X_samples, return_cov=True)
         means.append(mean)
@@ -170,7 +187,7 @@ def entropy_search(dataset):
         # A: This way the noise is the same for all IG iterations
         innovations = []
         logging.debug("Generating innovations..")
-        for _ in range(P):
+        for _ in range(n_innovations):
             # Generate a gaussian noise vector
             innovations.append(np.random.normal(size=n_gen_samples))
 
@@ -181,12 +198,21 @@ def entropy_search(dataset):
         Returns the information gain value for `test_point`
 
         """
-        a = 0
+        a, errors = 0, 0
+        testpoint_mu = np.zeros([len(models)])
+        testpoint_var = np.zeros([len(models)])
+        for i,m in enumerate(models):
+            testpoint_mu[i], testpoint_var[i] = m.predict(test_point.reshape(1, -1), return_cov=True)
+        
+        # **Predictive** variance (Hutter, 2013 - Algorithm Runtime Prediction: Methods & Evaluation)
+        # Section 4.2: Scaling to large amounts of data with approximate gaussian processes
+        pred_mean_testpoint = testpoint_mu.mean()
+        pred_var_testpoint = (testpoint_var + testpoint_mu**2).mean() - pred_mean_testpoint**2
+
         for i, model in enumerate(models):
-            _, testpoint_var = model.predict(test_point.reshape(1, -1), return_cov=True)
-            # vectorize
-            for p in range(P):
-                d_mu, d_sigma = compute_innovations(test_point.reshape(1, -1), model, representers[i], testpoint_var)
+            # TODO: can it be vectorized?
+            for p in range(n_innovations):
+                d_mu, d_sigma = compute_innovations(test_point.reshape(1, -1), model, representers[i], pred_var_testpoint.reshape(-1, 1))
 
                 # Compute pmin from the updated posterior
                 #q_min = compute_pmin(means[i] + d_mu.reshape(-1), covariances[i] + d_sigma)
@@ -200,7 +226,10 @@ def entropy_search(dataset):
                 d_entropy = - np.sum(np.exp(q_min) * (q_min + U[i])) + \
                     np.sum(np.exp(p_min[i][0]) * (p_min[i][0] + U[i]))
 
-                a += 1/P * d_entropy
+                if d_entropy != np.nan:
+                    a += 1/n_innovations * d_entropy
+                else:
+                    logging.warning("Cannot compute Information Gain with this model")
 
         logging.info(f"IG: {1/len(models) * a} for test point: {test_point}")  
         return 1/len(models) * a
@@ -225,16 +254,20 @@ def obj_function(configuration, dataset):
 
 def generate_prior(obj_function, dataset):
 
-    C = np.array([-5, 0, 5])
-    gamma = np.array([-5, 0, 5])
+    C = np.array([-8, 0, 8])
+    gamma = np.array([-8, 0, 8])
 
-    x_values, y_values = [], []
+    x_values, y_values = [], [
+        0.71234,  0.71234, 0.2154, 
+        0.71234, 0.71234, 0.6523, 
+        0.91266, 0.81256, 0.71234
+        ]
 
     for c in C:
         for g in gamma:
-            y = obj_function((c,g), dataset)
+            #y = obj_function((c,g), dataset)
             x_values.append((c,g))
-            y_values.append(y)
+            #y_values.append(y)
 
     return np.array(x_values), np.array(y_values)
 
