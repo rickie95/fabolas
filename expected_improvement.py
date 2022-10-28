89db19bfb09a89ed28befbd8069579b6ac0b6ebc
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2022-10-28 19:18:12 +0200

    adds prior loading and logging in EI

--- expected_improvement.py
+++ expected_improvement.py
@@ -5,6 +5,8 @@ from sklearn.gaussian_process.kernels import Matern
 
 from acquisitions import expected_improvement
 
+import logging
+
 
 def ei(obj_function, prior, bounds=None) -> np.array:
     """
@@ -25,26 +27,26 @@ def ei(obj_function, prior, bounds=None) -> np.array:
     n_iter = 10
 
     for i in range(n_iter):
-        print(f"---- EI: Iteration #{i} ----")
+        print(f"---- EI: Iteration #{i+1} ----")
 
-        X_candidate = get_candidate(prior)
+        X_candidate = get_candidate(prior, bounds)
 
-        print(f"Selected this candidate: {X_candidate}")
+        logging.info(f"Selected this candidate: {X_candidate}")
 
         if bounds is not None:
             for i, b in enumerate(bounds):
                 X_candidate[i] = np.clip(X_candidate[i], b[0], b[1])
 
+        logging.info("Evaluating function...")
         y_candidate = obj_function(X_candidate)
 
-        print(f"Function value: {y_candidate}")
+        logging.info(f"Function value: {y_candidate}")
 
         prior["X"] = np.vstack([prior["X"], X_candidate])
-        prior["y"] = np.append(prior["y"], np.array[y_candidate])
-
+        prior["y"] = np.append(prior["y"], np.array([y_candidate]))
 
     prior["y_best"] = max(prior["y"])
-    imax = np.amax(prior["y"])
+    imax = np.argmax(prior["y"])
     prior["X_best"] = prior["X"][imax]
 
     return prior
@@ -54,10 +56,9 @@ def get_candidate(prior, bounds):
     n_gen_samples = 100
 
     cov = 1
-    kernel = cov * Matern(nu=5/2)
+    kernel = cov * Matern(nu=5/2, length_scale_bounds=(1e-10, 1e10))
     gpr = GaussianProcessRegressor(kernel=kernel)
     gpr.fit(prior["X"], prior["y"])
-
 
     X_samples = np.random.uniform(
             low=[b[0] for b in bounds],
@@ -66,7 +67,7 @@ def get_candidate(prior, bounds):
         )
 
     y_pred = gpr.predict(prior["X"])
-    mean, covariance = gpr.predict(X_samples)
+    mean, covariance = gpr.predict(X_samples, return_cov=True)
 
     ei = expected_improvement(
         mean,
@@ -74,5 +75,5 @@ def get_candidate(prior, bounds):
         y_pred
     )
 
-    index = np.amax(ei)
+    index = np.argmax(ei)
     return X_samples[index]
--- experiments_svm.py
+++ experiments_svm.py
@@ -7,31 +7,55 @@ from sklearn.svm import SVC
 
 from datasets import load_mnist
 
+import pandas as pd
+import numpy as np
+
 import logging
+
 
 def obj_function(configuration, dataset=None):
     if dataset is None:
-        dataset = load_mnist()
+        dataset = load_mnist(training_size=1/64)
 
-    c, gamma = configuration
-    grid = GridSearchCV(SVC(kernel="rbf"), {'C': c, 'gamma': gamma}, n_jobs=-1, verbose=3, cv=5)
+    c, gamma = 10**configuration
+    grid = GridSearchCV(SVC(kernel="rbf"), {'C': [c], 'gamma': [gamma]}, n_jobs=-1, verbose=1, cv=5)
     grid.fit(dataset["X"], dataset["y"])
-    return grid.best_score_
+    return 1 - grid.best_score_
+
+
+def load_prior():
+    """
+        Returns a dictionary holding rows referring to previuos runs
+
+        "X": configurations, tuples of C and gamma values in log10 space (N, 2)
+        "size": training set size (0, 1]
+        "y": validation_error [0, 1]
+        "c": log10 cost of the run, expressed in seconds
+    """
+    df = pd.read_csv("./results/fabolas/prior.csv")
+    df = df.query("size > 0.20")
+
+    return {
+        "X": np.log10(np.array([df["C"], df["gamma"]]).T),
+        "size": np.array(df["size"]).reshape(-1, 1),
+        "y": 1 - np.array(df["validation_score"]).reshape(-1, 1),
+        "c": np.array(df["time_s"]).reshape(-1, 1)
+    }
 
 
 if __name__ == "__main__":
 
     logging.basicConfig(format='SVM_MNIST (%(process)s) - %(levelname)s - %(message)s', level=logging.INFO)
-    #TODO: some steps to perform:
+    # TODO: some steps to perform:
     # load prior
     # set bounds
     # perform hyper opt
     # print/save results
 
-    # prior = load_prior()
-    prior = generate_prior()
+    prior = load_prior()
+    # prior = generate_prior()
 
-    bounds = [(-10, 10),(-10, 10)]
+    bounds = [(-10, 10), (-10, 10)]
     results = []
 
     method = 'expected_improvement'
