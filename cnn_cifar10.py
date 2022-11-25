7420b1fd98863a967c58eb788719ddd574255509
Riccardo Malavolti <riccardo.malavolti@stud.unifi.it>
2022-11-25 12:03:12 +0100

    adds print

--- cnn_cifar10.py
+++ cnn_cifar10.py
@@ -129,7 +129,8 @@ def load_prior(with_size=1):
 
 def cnn_cifar10(method='random_search', save_path=None):
     save_path = "./results/cnn_cifar10" if save_path is None else save_path
-
+    print(f"Log will be found at {save_path}/cnn_cifar10_{datetime.datetime.now()}.log")
+    
     logging.basicConfig(
         format='CNN_CIFAR10 (%(process)s) - %(levelname)s - %(message)s',
         level=logging.INFO,
