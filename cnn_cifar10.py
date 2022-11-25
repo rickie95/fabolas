b277c2d0c089fc53ac0ea771eee1186a3a4284b6
Riccardo Malavolti <riccardo.malavolti@stud.unifi.it>
2022-11-25 11:57:22 +0100

    fix argv

--- cnn_cifar10.py
+++ cnn_cifar10.py
@@ -201,11 +201,11 @@ if __name__ == "__main__":
 
     print(f"Passed {len(sys.argv)} arguments:")
 
-    if len(sys.argv) == 2:
+    if len(sys.argv) > 0:
         method = sys.argv[1]
         print(f"method={sys.argv[1]}")
 
-    if len(sys.argv) == 3:
+    if len(sys.argv) > 1:
         save_path = sys.argv[2]
         print(f"save_path={sys.argv[2]}")
 
