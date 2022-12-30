ddedabbda41346f1bb1f714f3f1d6502fd277dc1
Riccardo Malavolti <riccardo.malavolti@stud.unifi.it>
2022-12-30 15:02:42 +0100

    add svhn script, also priors

---
+++ cnn_svhn.py
@@ -0,0 +1,232 @@
+"""
+    Performs experiments on a CNN trained on SVHN
+"""
+
+import logging
+import sys
+import datetime
+import math
+
+import numpy as np
+import pandas as pd
+
+from datasets import load_svhn
+from utils import save_results, print_usage
+
+from keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+
+from random import sample
+
+def obj_function(configuration):
+    """
+    Model building, the CNN has the following properties:
+    * 3 convolutional layers, each one with batch normalization
+    * optimized using Adam
+    * a dense layer
+
+    Returns the validation error and the time spent.
+    """
+
+    training_set_size = 1
+
+    assert 4 < len(
+        configuration) < 7, "Configuration must hold 5 parameters (6 if running FABOLAS)"
+
+    l1_filters, l2_filters, l3_filters, batch_size, learning_rate = configuration[0:5]
+
+    if len(configuration) == 6:
+        training_set_size = configuration[-1]
+
+    # Remap log scale parameters
+    l1_filters = (int) (2**np.round(l1_filters))
+    l2_filters = (int) (2**np.round(l2_filters))
+    l3_filters = (int) (2**np.round(l3_filters))
+
+    batch_size = (int) (np.round(batch_size))
+
+    learning_rate = 10**learning_rate
+
+    model = Sequential()
+
+    # Layer 1
+    model.add(Conv2D(l1_filters, (5, 5), input_shape=(
+        32, 32, 3), activation="relu"))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1), padding="same"))
+
+    # Layer 2
+    model.add(Conv2D(l2_filters, (5, 5), input_shape=(
+        32, 32, 3), activation="relu"))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1), padding="same"))
+
+    # Layer 3
+    model.add(Conv2D(l3_filters, (5, 5), input_shape=(
+        32, 32, 3), activation="relu"))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1), padding="same"))
+    model.add(Flatten())
+    model.add(Dense(10, activation="softmax"))
+
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer="adam",
+        metrics=["accuracy"]
+    )
+
+    dataset = load_svhn()
+
+    indices = sample(range(0, dataset["X"].shape[0]), 
+        math.floor(dataset["X"].shape[0] * training_set_size))
+
+    model.summary()
+    history = model.fit(
+        dataset["X"][indices],
+        dataset["y"][indices], 
+        epochs=40, 
+        batch_size=batch_size,
+        verbose=1,
+        validation_data=(dataset["X_test"], dataset["y_test"])
+        )
+
+    val_accuracy = history.history['val_accuracy'][-1]
+
+    return val_accuracy
+
+
+def generate_prior(bounds, n_points=25):
+    prior = {
+        "X": np.empty((0, 2)),
+        "y": np.empty((0, 1)),
+        "s": np.empty((0, 1)),
+        "c": np.empty((0, 1))
+    }
+
+    prior_candidates = np.random.uniform(
+        low=[b[0] for b in bounds],
+        high=[b[1] for b in bounds],
+        size=(n_points, len(bounds)))
+
+    for configuration in prior_candidates:
+        y, c = obj_function(configuration)
+        prior["X"] = np.vstack([prior["X"], np.array(configuration)])
+        prior["y"] = np.append(prior["y"], np.array([y]))
+        prior["s"] = np.append(prior["s"], np.array([y]))
+        prior["c"] = np.append(prior["c"], np.array([y]))
+
+
+def load_prior(with_size=False):
+    if not with_size:
+        df = pd.read_csv("./prior/prior_cnn_svhn.csv")
+    else:
+        df = pd.read_csv("./prior/prior_cnn_svhn_size.csv")
+
+    return {
+        "X": np.concatenate(
+            (np.log2(
+                np.array([df["l1_filters"],
+                          df["l2_filters"],
+                          df["l3_filters"]])
+            ),
+                np.array([df["batch_size"]]),
+                np.log10(np.array([df["learn_rate"]]))),
+            axis=0).T,
+        "y": np.array(df["score"]).reshape(-1, 1),
+        "c": np.array(df["cost_s"]).reshape(-1, 1),
+        "size": np.array(df["size"]).reshape(-1, 1)
+    }
+
+
+def cnn_svhn(method='random_search', save_path=None):
+    save_path = "./results/cnn_svhn" if save_path is None else save_path
+    print(f"Log will be found at {save_path}cnn_svhn_{datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.log")
+
+    logging.basicConfig(
+        format='CNN_SVHN (%(process)s) - %(levelname)s - %(message)s',
+        level=logging.INFO,
+        filename=f"{save_path}cnn_svhn_{datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.log",
+        filemode="w",
+        force=True
+    )
+
+    prior = None
+
+    method = 'random_search' if method is None else method
+
+    try:
+        prior = load_prior()
+    except:
+        prior = generate_prior()
+
+    assert (prior is not None)
+
+    bounds = [
+        (4, 9),
+        (4, 9),
+        (4, 9),
+        (32, 512),
+        (-6, -1)
+    ]
+
+    results = []
+
+    if method == 'random_search':
+        from random_search import rs
+        logging.info("Starting Random Search...")
+        results, progress = rs(obj_function, prior, bounds)
+
+    elif method == 'expected_improvement':
+        from expected_improvement import ei
+        logging.info("Starting Expected Improvement...")
+        results, progress = ei(obj_function, prior, bounds)
+
+    elif method == 'entropy_search':
+        from entropy_search import es
+        logging.info("Starting Entropy Search...")
+        results, progress = es(obj_function, prior, bounds)
+
+    elif method == 'fabolas':
+        from fabolas import fabolas
+        logging.info("Starting FABOLAS...")
+        bounds.append((1/256, 1))
+        results, progress = fabolas(obj_function, load_prior(
+            with_size=True), bounds)
+
+    else:
+        return 1
+
+    y_best = min(results["y"])
+    imax = np.argmin(results["y"])
+    x_best = results["X"][imax]
+
+    logging.info(f"Best value: {y_best}, with conf {x_best}")
+
+    save_results(
+        results,
+        progress,
+        method,
+        save_path
+    )
+
+    return 0
+
+
+if __name__ == "__main__":
+    method = None
+    save_path = None
+
+    print(f"Passed {len(sys.argv)} arguments:")
+
+    if len(sys.argv) > 1:
+        method = sys.argv[1]
+        print(f"method={sys.argv[1]}")
+
+    if len(sys.argv) > 2:
+        save_path = sys.argv[2]
+        print(f"save_path={sys.argv[2]}")
+
+    if cnn_svhn(method=method, save_path=save_path) > 0:
+        print_usage(sys.argv)
--- datasets.py
+++ datasets.py
@@ -29,7 +29,7 @@ def load_mnist(training_size=1):
     test_x = test_x.reshape(test_x.shape[0], test_x.shape[1] * test_x.shape[2])
 
     if training_size < 1:
-        indices = sample(range(1, train_x.shape[0]), math.floor(train_x.shape[0]*training_size))
+        indices = sample(range(0, train_x.shape[0]), math.floor(train_x.shape[0]*training_size))
         dataset["X"] = train_x[indices]
         dataset["y"] = train_y[indices]
     else:
@@ -71,5 +71,61 @@ def load_cifar(train_size=1):
 
     dataset["X_test"] = X_test/255
     dataset["y_test"] = to_categorical(Y_test, 10)
+
+    return dataset
+
+
+def load_svhn():
+    """
+        Loads the Street View House Numbers dataset.
+    """
+    from scipy.io import loadmat
+    from sklearn.preprocessing import LabelBinarizer
+    import numpy as np
+    from random import shuffle
+
+    train_raw = loadmat('./data/svhn/train_32x32.mat')
+    test_raw = loadmat('./data/svhn/test_32x32.mat')
+
+    train_images = np.array(train_raw['X'])
+    test_images = np.array(test_raw['X'])
+
+    train_labels = train_raw['y']
+    test_labels = test_raw['y']
+    train_images = np.moveaxis(train_images, -1, 0)
+    test_images = np.moveaxis(test_images, -1, 0)
+
+    train_images = train_images.astype('float64') / 255.0
+    test_images = test_images.astype('float64') / 255.0
+
+    train_labels = train_labels.astype('int64')
+    test_labels = test_labels.astype('int64')
+
+    lb = LabelBinarizer()
+    train_labels = lb.fit_transform(train_labels)
+    test_labels = lb.fit_transform(test_labels)
+
+    # 73257 images
+    # 6000 of them used as validation set
+
+    inds = [x for x in range(train_images.shape[0])]
+    shuffle(inds)
+
+    train_ind = inds[0:67257]
+    valid_ind = inds[67257:-1]
+
+    dataset = {
+        'X' : train_images[train_ind],
+        'y' : train_labels[train_ind],
+        'X_test' : train_images[valid_ind],
+        'y_test' : train_labels[valid_ind]
+    }
+
+    del train_raw
+    del test_raw
+    del train_images
+    del train_labels
+    del train_ind
+    del valid_ind
 
     return dataset
